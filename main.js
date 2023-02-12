3d55185c2eb4182b297c267e31db6d3529bf9ec7
Matthew R. Stennett <matt@hyperhedronsystems.com>
2023-02-12 00:01:47 -0600

    Cache map

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 class PromiseMemoizationDemo {
-    // Caching a promise
-    cache = null;
+    // Caching promises
+    cache = new Map();
 
     getData (url) {
-        if(this.cache === null) {
+        if(!this.cache.has(url)) {
             // For demonstration purposes, the cache holds both a Promise and its return value in memory,
             // but the return value could be persisted elsewhere, e.g. localStorage if not terribly sensitive.
             // However, always at least caching a Promise in memory and returning it allows us to respond to multiple
             // widgets with the response from only a single fetch request, even if the request hasn't finished yet.
             // This is called "Promise Memoization."
-            this.cache = new Promise((resolve) => {
+            this.cache.set(url, new Promise((resolve) => {
                 console.log("Making request.");
                 // Let's simulate request latency
                 setTimeout( () => {
@@ -19,12 +19,12 @@ class PromiseMemoizationDemo {
                 // Then let's simulate a cache expiration in thirty seconds
                 let self = this;
                 setTimeout( () => {
-                    self.cache = null;
+                    self.cache.delete(url);
                     console.log("Cache cleared.");
                 }, 30000);
-            });
+            }));
         }
-        return this.cache;
+        return this.cache.get(url);
     }
 }
 
